95fc6dce12cf142156c4f8e0e0198657bc9188e5
Zach Daniels <zach.daniels91@gmail.com>
2019-04-16 15:49:17 -0700

    Prototype auth styling & basic state management for sign-in

--- components/screens/SettingsScreen.js
+++ components/screens/SettingsScreen.js
@@ -1,14 +1,188 @@
-import React from 'react';
-import { ExpoConfigView } from '@expo/samples';
+import React from 'react'
+
+import {
+  AsyncStorage,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  StyleSheet,
+  Text,
+  SafeAreaView,
+  StatusBar,
+  KeyboardAvoidingView,
+  Keyboard,
+  View,
+  Alert
+} from 'react-native'
+
+import {
+  Container,
+  Item,
+  Input,
+  Icon,
+} from 'native-base'
 
 export default class SettingsScreen extends React.Component {
-  static navigationOptions = {
-    title: 'app.json',
+  state = {
+    password1: '',
+    password2: '',
+  }
+  onChangeText = (key, value) => {
+    this.setState({[key]: value})
+  }
+   
+  _signOutAsync = async () => {
+    await AsyncStorage.clear();
+    this.props.navigation.navigate('Auth');
+    console.log("signed out!")
   };
 
   render() {
-    /* Go ahead and delete ExpoConfigView and replace it with your
-     * content, we just wanted to give you a quick view of your config */
-    return <ExpoConfigView />;
+    return (
+      <SafeAreaView style={styles.container}>
+        <StatusBar/>
+        <KeyboardAvoidingView style={styles.container} behavior='padding' enabled>
+          <TouchableWithoutFeedback style={styles.container} onPress={Keyboard.dismiss}>
+            <View style={styles.container}>
+              {/*Infos*/}
+              <Container style={styles.infoContainer}>
+                <View style={styles.container}>
+                  <View 
+                    style={
+                      [styles.buttonStyle, {borderRadius: 4, marginBottom: 20}]
+                    }>
+                    <Text style={styles.buttonText}>Change password</Text>              
+                  </View>
+                  {/* Old password */}
+                  <Item rounded style={styles.itemStyle}>
+                    <Icon
+                      active
+                      name='lock'
+                      style={styles.iconStyle}
+                    />
+                    <Input
+                      style={styles.input}
+                      placeholder='Old password'
+                      placeholderTextColor='#adb4bc'
+                      returnKeyType='next'
+                      autoCapitalize='none'
+                      autoCorrect={false}
+                      secureTextEntry={true}
+                      onSubmitEditing={(event) => { this.refs.SecondInput._root.focus()}}
+                      onChangeText={value => this.onChangeText('password1', value)}
+                    />
+                  </Item>    
+                  {/* New password */}              
+                  <Item rounded style={styles.itemStyle}>
+                    <Icon
+                      active
+                      name='lock'
+                      style={styles.iconStyle}
+                    />
+                    <Input
+                      style={styles.input}
+                      placeholder='New password'
+                      placeholderTextColor='#adb4bc'
+                      returnKeyType='go'
+                      autoCapitalize='none'
+                      autoCorrect={false}
+                      secureTextEntry={true}
+                      ref='SecondInput'
+                      onChangeText={value => this.onChangeText('password2', value)}
+                    />
+                  </Item>
+                  <TouchableOpacity
+                    style={styles.buttonStyle}>
+                    <Text style={styles.buttonText}>
+                      Submit
+                    </Text>
+                  </TouchableOpacity>
+                  <View 
+                    style={
+                      {
+                        justifyContent: 'center', 
+                        alignItems: 'center', 
+                        paddingBottom: 100
+                      }
+                    }
+                   />
+                  <TouchableOpacity
+                    style={
+                      [styles.buttonStyle, 
+                        {
+                          flexDirection: 'row', 
+                          justifyContent: 'center'
+                        }
+                      ]
+                    }
+                    onPress={() => this._signOutAsync()}>
+                    <Icon name='md-power' style={{color: '#fff', paddingRight: 10}}/>
+                    <Text style={styles.buttonText}>
+                      Sign out
+                    </Text>
+                  </TouchableOpacity>
+                </View>
+              </Container>
+            </View>
+          </TouchableWithoutFeedback>
+        </KeyboardAvoidingView>
+      </SafeAreaView>
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#aa73b7',
+    justifyContent: 'center',
+    flexDirection: 'column'
+  },
+  input: {
+    flex: 1,
+    fontSize: 17,
+    fontWeight: 'bold',
+    color: '#5a52a5',
+  },
+  infoContainer: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    height: 350,
+    bottom: 25,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 30,
+    backgroundColor: '#aa73b7',
+  },
+  itemStyle: {
+    marginBottom: 20,
+  },
+  iconStyle: {
+    color: '#5a52a5',
+    fontSize: 28,
+    marginLeft: 15
+  },
+  buttonStyle: {
+    alignItems: 'center',
+    backgroundColor: '#667292',
+    padding: 14,
+    marginBottom: 20,
+    borderRadius: 24,
+  },
+  buttonText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: "#fff",
+  },
+  logoContainer: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    height: 400,
+    bottom: 180,
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+  },
+})
--- components/screens/SignInScreen.js
+++ components/screens/SignInScreen.js
@@ -1,16 +1,39 @@
 import React from 'react';
+
 import {
   StyleSheet,
   Text,
   View,
-  Button,
-  AsyncStorage
+  TouchableOpacity,
+  AsyncStorage,
+  TouchableWithoutFeedback,
+  SafeAreaView,
+  StatusBar,
+  KeyboardAvoidingView,
+  Keyboard,
+  Alert,
+  Animated
 } from 'react-native';
+
+import {
+  Container,
+  Item,
+  Input,
+  Icon
+} from 'native-base'
 
 export default class SignInScreen extends React.Component {
   static navigationOptions = {
     header: null,
   };
+
+  state = {
+    username: '',
+    password: '',
+  }
+  onChangeText = (key, value) => {
+    this.setState({[key]: value})
+  }
 
   _signInAsync = async () => {
       try {
@@ -24,22 +47,133 @@ export default class SignInScreen extends React.Component {
 
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Welcome to the Sign-in Screen!</Text>
-        <Button title="Sign in!" onPress={this._signInAsync} />
-        <Button title="Sign up!" onPress={ () => this.props.navigation.navigate('SignUp')} />
-        <Button title="Forgot Password" onPress={ () => this.props.navigation.navigate('ForgotPassword')} />
-      </View>
-    );
+      <SafeAreaView style={styles.container}>
+        <StatusBar/>
+        <KeyboardAvoidingView style={styles.container} behavior='padding' enabled>
+          <TouchableWithoutFeedback style={styles.container} onPress={Keyboard.dismiss}>
+            <View style={styles.container}>
+              <Container style={styles.infoContainer}>
+                <View style={styles.container}>
+                  <Item rounded style={styles.itemStyle}>
+                    <Icon
+                      active
+                      name='person'
+                      style={styles.iconStyle}
+                    />
+                    <Input
+                      style={styles.input}
+                      placeholder='Username'
+                      placeholderTextColor='#adb4bc'
+                      keyboardType={'email-address'}
+                      returnKeyType='next'
+                      autoCapitalize='none'
+                      autoCorrect={false}
+                      onSubmitEditing={(event) => {this.refs.SecondInput._root.focus()}}
+                      onChangeText={value => this.onChangeText('username', value)}
+                    />
+                  </Item>
+                  <Item rounded style={styles.itemStyle}>
+                    <Icon
+                      active
+                      name='lock'
+                      style={styles.iconStyle}
+                    />
+                    <Input
+                      style={styles.input}
+                      placeholder='Password'
+                      placeholderTextColor='#adb4bc'
+                      returnKeyType='go'
+                      autoCapitalize='none'
+                      autoCorrect={false}
+                      secureTextEntry={true}
+                      ref='SecondInput'
+                      onChangeText={value => this.onChangeText('password', value)}
+                    />
+                  </Item>
+                  <TouchableOpacity
+                    onPress={() => this._signInAsync()}
+                    style={styles.buttonStyle}>
+                    <Text style={styles.buttonText}>
+                      Sign In
+                    </Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity
+                    onPress={() => this.props.navigation.navigate('SignUp')}
+                    style={styles.buttonStyle}>
+                    <Text style={styles.buttonText}>
+                      Sign Up
+                    </Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity
+                    onPress={() => this.props.navigation.navigate('ForgotPassword')}
+                    style={styles.buttonStyle}>
+                    <Text style={styles.buttonText}>
+                      Forgot Password
+                    </Text>
+                  </TouchableOpacity>
+                </View>
+              </Container>
+            </View>
+          </TouchableWithoutFeedback>
+        </KeyboardAvoidingView>
+      </SafeAreaView>
+    )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    textAlign: 'center',
+    backgroundColor: '#aa73b7',
     justifyContent: 'center',
-    backgroundColor: '#fff',
-  }
-});
+    flexDirection: 'column'
+  },
+  input: {
+    flex: 1,
+    fontSize: 17,
+    fontWeight: 'bold',
+    color: '#5a52a5',
+  },
+  infoContainer: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    height: 350,
+    bottom: 25,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 30,
+    backgroundColor: '#aa73b7',
+  },
+  itemStyle: {
+    marginBottom: 20,
+  },
+  iconStyle: {
+    color: '#5a52a5',
+    fontSize: 28,
+    marginLeft: 15
+  },
+  buttonStyle: {
+    alignItems: 'center',
+    backgroundColor: '#667292',
+    padding: 14,
+    marginBottom: 20,
+    borderRadius: 24,
+  },
+  buttonText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: "#fff",
+  },
+  logoContainer: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    height: 400,
+    bottom: 180,
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+  },
+})
