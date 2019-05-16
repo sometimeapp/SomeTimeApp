a55e565cd6a401ca7db2c7644f1d7150d9504671
Jonathan Adler <jradler@gmail.com>
2019-05-16 16:54:55 -0700

    Initial validation of QR code working.  Recognizes invalide codes and legit Pledges with empty terms.  Alerts and redirects Home, but transition is not smooth.

--- components/screens/QRScannerScreen.js
+++ components/screens/QRScannerScreen.js
@@ -33,10 +33,37 @@ export default class QRScannerScreen extends React.Component {
         );
     }
 
+    parseAndValidateJSON = (str) => {
+        //check for null input
+        if (!(!!str)) return false;
+        //check that input is a string
+        if( typeof( str ) !== 'string' ) return false; 
+
+        let json;
+        try {
+            json = JSON.parse(str)
+        } catch (e) {
+            return false;
+        }
+        //once parsed, check that it is NOT an array
+        if( json instanceof Array ){
+            console.log("array");
+            return false;
+        } 
+
+        return json;
+    }
+
     handleBarCodeScanned = ({ type, data }) => {
         //alert(`Bar code with type ${type} and data is ${typeof data} `);
-        data = JSON.parse(data);
-        if(data.screen) {
+        data = this.parseAndValidateJSON(data);
+        if(!data || !data.promisorID) {
+            this.props.navigation.navigate('Home');
+            
+        } else if(!data.terms) {
+            alert("cannot accept a promise with empty terms");
+            this.props.navigation.navigate('Home');
+        } else if(data.screen) {
             this.props.navigation.navigate('ResolveReview', data);
         } else {
             this.props.navigation.navigate('Review', data);
